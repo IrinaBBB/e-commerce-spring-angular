cff708b70b88325b4e6873a9fd08d47d85ea8b2c
IrinaBBB <balkarovairina@gmail.com>
2023-05-28 20:26:11 +0200

    Add html-template for Product component

--- spring-boot-ecommerce/src/main/java/ru/aurorahost/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/ru/aurorahost/ecommerce/dao/ProductRepository.java
@@ -2,9 +2,10 @@ package ru.aurorahost.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import ru.aurorahost.ecommerce.entity.Product;
 
 @Repository
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
 }
