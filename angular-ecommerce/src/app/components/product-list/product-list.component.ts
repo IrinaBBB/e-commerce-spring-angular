dcd59142cfbe04b0c902ec1839bb5778b50f0ff2
IrinaBBB <balkarovairina@gmail.com>
2023-05-28 12:31:33 +0200

    Add Product service in Angular

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+    selector: 'app-product-list',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent {
+    products: Product[] = [];
 
+    constructor(private productService: ProductService) {}
+
+    ngOnInit(): void {
+        this.listProducts();
+    }
+
+    private listProducts() {
+        this.productService
+            .getProductList()
+            .subscribe((data) => (this.products = data));
+    }
 }
